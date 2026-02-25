47644125e3a2bb4c20a80f79a34680aff0c5592e
ringo88888 <ringomaisa127@gmail.com>
2026-02-25 13:40:34 +0900

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,184 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='text-gray-700 border-b border-gray-200'>
+        <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+          <a href="#" className='font-medium text-gray-900 mb-4 md:mb-0'>
+            <span className='text-xl ml-3'>Naka Saya</span>
+          </a>
+          <nav className='md:ml-auto text-base'>
+            <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>Home</a>
+            <a href="#about" className='mr-5 hover:text-blue-400 duration-300'>About</a>
+            <a href="#skills" className='mr-5 hover:text-blue-400 duration-300'>Skills</a>
+            <a href="#blog" className='hover:text-blue-400 duration-300'>Blog</a>
+          </nav>
+        </div>
+      </header>
+      <section className='text-gray-700' id="home">
+        <div className='flex container mx-auto py-20 px-5 flex-col md:flex-row items-center'>
+          <div className='md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left'>
+            <h1 className='text-3xl sm:text-6xl text-gray-900 font-medium mb-4'>Naka Saya<br />
+              My Portfolio Website</h1>
+            <p className='mb-8 leading-relaxed'>現在、webエンジニアとしてデビューしたてのほやほやです。得意な言語はHTML, CCS, JavaScript, TypeScript, PHP, SQL, Reactなどweb全般とPythonです。自身が作ったものを公開しておりますので、ぜひ見ていってください。</p>
+            <button className='text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300'>Contact Me</button>
+          </div>
+          <div className='md:w-1/2 lg:max-w-lg w-5/6'>
+            <img src="./img/myPicture.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section className='text-gray-700 border-t border-gray-200'>
+        <div className='container px-5 py-24 mx-auto'>
+          <div className='text-center mb-20'>
+            <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900'>About Me</h1>
+            <p className='pb-10'>これまで、マーケティング業界に数年財務分析を行っていました。現在はIT業界でシステム構築に従事しながら副業としてwebサイトやwebAppを個人として受注しています。また投資やコンサルティングも積極的に行っており、ご相談されたい方はコンタクトお待ちしています。
+            </p>
+            <p>いかに個人としての成果物を公開いています。主にSNSなどのChatアプリの構築をReactのJSやTSで実装しています。また、Pythonを使ってAIによる株価の予測サイトを構築しました。また数件の事務所のwebサイトの構築など多岐にわたります。</p>
+          </div>
+          {/* カードのdivタグ */}
+          <div className='flex flex-wrap'>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3 3H11V11H3V3Z" fill="currentColor" />
+                      <path d="M3 13H11V21H3V13Z" fill="currentColor" />
+                      <path d="M13 3H21V11H13V3Z" fill="currentColor" />
+                      <path d="M13 13H21V21H13V13Z" fill="currentColor" />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>作品名</h2>
+                </div>
+                <div>
+                  <p>説明文例：ReactとJavaScriptで投稿機能つきのChatアプリ、仕様はReact, JavaScript, html, css, firebase, AWS, ログイン・ログアウト認証機能、メッセージ投稿機能、検索機能、お気にいり登録機能を実装しています。</p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る　→</a>
+                </div>
+              </div>
+            </div>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7ZM9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M5 1C2.79086 1 1 2.79086 1 5V19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5ZM19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>作品名</h2>
+                </div>
+                <div>
+                  <p>説明文</p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る　→</a>
+                </div>
+              </div>
+            </div>
+            <div className='md:w-1/3 p-4'>
+              <div className='bg-gray-100 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full'>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.5 2C6.84315 2 5.5 3.34315 5.5 5C5.5 6.65685 6.84315 8 8.5 8H15.5C17.1569 8 18.5 6.65685 18.5 5C18.5 3.34315 17.1569 2 15.5 2H8.5Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M15.5 9C13.8431 9 12.5 10.3431 12.5 12C12.5 13.6569 13.8431 15 15.5 15C17.1569 15 18.5 13.6569 18.5 12C18.5 10.3431 17.1569 9 15.5 9Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M5.5 12C5.5 10.3431 6.84315 9 8.5 9H11.5V15H8.5C6.84315 15 5.5 13.6569 5.5 12Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M8.5 16C6.84315 16 5.5 17.3431 5.5 19C5.5 20.6569 6.84315 22 8.5 22C10.1569 22 11.5 20.6569 11.5 19V16H8.5Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </div>
+                  <h2 className='text-gray-900 text-lg font-medium ml-2'>作品名</h2>
+                </div>
+                <div>
+                  <p>説明文</p>
+                  <a href="#" className='mt-3 text-green-500 items-center'>もっと見る　→</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='text-gray-700 border-t border-gray-200'>
+        <div className='container px-5 py-24 mx-auto flex flex-wrap'>
+          {/* left side */}
+          <div className='mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center'>
+            <img src="./img/pc.jpg" alt="" className='rounded' />
+          </div>
+          {/* rightside */}
+          <div className='lg:pl-12 lg:py-6 w-full lg:w-1/2'>
+            <h1 className='text-2x1 sm:text-3xl font-medium text-gray-900 mb-10 text-center lg:text-left'>My Skills</h1>
+            <div>
+              <h2>HTML</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-green-600 text-xs leading-none py-1 text-center text-white ' style={{ width: "85%" }}>85%</div>
+              </div>
+              <h2>JavaScript</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-orange-600 text-xs leading-none py-1 text-center text-white ' style={{ width: "75%" }}>75%</div>
+              </div>
+              <h2>React</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-blue-600 text-xs leading-none py-1 text-center text-white ' style={{ width: "50%" }}>50%</div>
+              </div>
+              <h2>Python</h2>
+              <div className='shadow bg-green-100 mt-2 w-full'>
+                <div className='bg-yellow-600 text-xs leading-none py-1 text-center text-white ' style={{ width: "10%" }}>10%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+
+  )
+}
+
+export default App
